886c7984731a2650b5a238466ede4114db0ed506
minhvuongrbs <minhvuong2810@gmail.com>
2018-04-01 09:37:04 +0700

    manage room

---
+++ TroTot/app/src/main/java/trotot/dnvn/cndd/trotot/Menu3Fragment.java
@@ -0,0 +1,27 @@
+package trotot.dnvn.cndd.trotot;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Menu3Fragment extends Fragment {
+
+
+    public Menu3Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu3, container, false);
+    }
+
+}
